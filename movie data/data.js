9569fe9500cd40f590ec1132780e55747f87e659
Бекзат <samsungalaxyj77715@gmail.com>
2022-02-22 01:19:44 +0600

    first commit

---
+++ movie data/data.js
@@ -0,0 +1,53 @@
+var formData = {
+    "Login": "",
+    "Password": "",
+    "Name": "Peter",
+    "Date": null,
+    "Country": "",
+    "City": "",
+    "Address": "",
+    "Phone": "",
+    "Accepted": false
+};
+var film = [{
+    "Название": "The Grand Budapest Hotel",
+    "Жанр": "Комедия",
+    "Дата выхода": "12-12-2014",
+    "Бюджет": 23000000,
+    "Цена за просмотр": 123,
+    "Страна": "Argentina"
+}, {
+    "Название": "Venom",
+    "Жанр": "ужасы",
+    "Дата выхода": "04-10-2018",
+    "Бюджет": 100000000,
+    "Цена за просмотр": 267,
+    "Страна": "USA"
+},{
+    "Название": "The Shawshank Redemption",
+    "Жанр": "Мелодрамма",
+    "Дата выхода": "12-12-1994",
+    "Бюджет": 2000000,
+    "Цена за просмотр": 99,
+    "Страна": "USA",
+    "Описание":"«Fear can hold you prisoner. Hope can set you free.»"
+},{
+    "Название": "The Green Mile",
+    "Жанр": "Мелодрамма",
+    "Дата выхода": "12-06-1999",
+    "Бюджет": 1000000,
+    "Цена за просмотр": 55,
+    "Страна": "USA",
+    "Описание":"«Miracles do happen. Miracles happen in the most unexpected places»"
+},{
+    "Название": "1+1",
+    "Жанр": "Комедия",
+    "Дата выхода": "04-02-2011",
+    "Бюджет": 1500000,
+    "Цена за просмотр": 100,
+    "Страна": "France",
+    "Описание":"«Sometimes you have to reach into someone else's world to find out what's missing in your own»"
+}];
+var genre = ['Comedy','Melodrama','Thriller','Horror','Action','Art-house'];
+
+var countries = ['Afghanistan','Albania','Algeria','Andorra','Angola','Antigua and Barbuda','Argentina','Armenia','Australia','Austria','Azerbaijan','The Bahamas','Bahrain','Bangladesh','Barbados','Belarus','Belgium','Belize','Benin','Bhutan','Bolivia','Bosnia and Herzegovina','Botswana','Brazil','Brunei','Bulgaria','Burkina Faso','Burma','Burundi','Cambodia','Cameroon','Canada','Cape Verde','Central African Republic','Chad','Chile','China','Colombia','Comoros','Democratic Republic of the Congo','Republic of the Congo','Costa Rica','Ivory Coast','Croatia','Cuba','Cyprus','Czech Republic','Denmark','Djibouti','Dominica','Dominican Republic','East Timor','Ecuador','Egypt','El Salvador','Equatorial Guinea','Eritrea','Estonia','Ethiopia','Fiji','Finland','France','Gabon','The Gambia','Georgia','Germany','Ghana','Greece','Grenada','Guatemala','Guinea','Guinea-Bissau','Guyana','Haiti','Honduras','Hungary','Iceland','India','Indonesia','Iran','Iraq','Republic of Ireland','Israel','Italy','Jamaica','Japan','Jordan','Kazakhstan','Kenya','Kiribati','North Korea','South Korea','Kuwait','Kyrgyzstan','Laos','Latvia','Lebanon','Lesotho','Liberia','Libya','Liechtenstein','Lithuania','Luxembourg','Republic of Macedonia','Madagascar','Malawi','Malaysia','Maldives','Mali','Malta','Marshall Islands','Mauritania','Mauritius','Mexico','Federated States of Micronesia','Moldova','Monaco','Mongolia','Montenegro','Morocco','Mozambique','Namibia','Nauru','Nepal','Kingdom of the Netherlands','New Zealand','Nicaragua','Niger','Nigeria','Norway','Oman','Pakistan','Palau','State of Palestine','Panama','Papua New Guinea','Paraguay','Peru','Philippines','Poland','Portugal','Qatar','Romania','Russia','Rwanda','Saint Kitts and Nevis','Saint Lucia','Saint Vincent and the Grenadines','Samoa','San Marino','São Tomé and Príncipe','Saudi Arabia','Senegal','Serbia','Seychelles','Sierra Leone','Singapore','Slovakia','Slovenia','Solomon Islands','Somalia','South Africa','South Sudan','Spain','Sri Lanka','Sudan','Suriname','Swaziland','Sweden','Switzerland','Syria','Tajikistan','Tanzania','Thailand','Togo','Tonga','Trinidad and Tobago','Tunisia','Turkey','Turkmenistan','Tuvalu','Uganda','Ukraine','United Arab Emirates','United Kingdom','United States','Uruguay','Uzbekistan','Vanuatu','Vatican City','Venezuela','Vietnam','Yemen','Zambia','Zimbabwe'];
